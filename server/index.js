f3b5d9e2db75c36f72342c44cc100300e3f43c1d
Rebekah Cruz <rebekah.cruz702@gmail.com>
2021-08-30 15:28:42 -0500

    created express server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const router = express.Router();
 
 //headers to allows CORS requests
 const headers = {
@@ -25,55 +28,73 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+app.use(express.static(`http://localhost:${port}/index.html`));
+app.use(express.json());
 
-const handleRequest = function (req, res) {
-  console.log(`Endpoint: ${req.url} Method: ${req.method}`);
 
-  // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
-  if (req.url == '/') {
-    console.log('redirecting');
-    res.writeHead(301, { ...headers, Location: `http://localhost:${port}/quote` }) //redirect to quote
-    res.end();
-  }
+app.get('/', (req, res)=> {
+  console.log('redirecting');
+  res.writeHead(301, { ...headers, Location: `http://localhost:${port}/quote` }) //redirect to quote
+  res.end();
+})
+app.get('/quote', (req, res) => {
+  var randomIndex = getRandomInt(0, quotes.length)
+  res.status(200).send(quotes[randomIndex])
+ })
 
-  // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
-    console.log('get request triggered!')
-    var randomIndex = getRandomInt(0, quotes.length)
+ app.post('/quote', (req, res) => {
+   quotes.push(req.body.quote)
+   res.status(201).send('New quote successfully added!')
+ })
 
-    req.on('data', (data) => {
-      res.writeHead(200)
-    })
-    req.on('end', () => {
-      res.end(quotes[randomIndex]);
-    });
-  }
+app.listen(port, () => {
+  console.log('Server is running in the terminal!');
+  console.log(`Listening on http://localhost:${port}`);
+})
 
-  // TODO: POST/CREATE
-  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
 
-    var data = '';
-    req.on('data', (chunkOfData) => {
-      data += chunkOfData.toString();
-    })
-    req.on('end', () => {
-      var quote = JSON.parse(data);
-      console.log('this is the data!', data);
-      quotes.push(quote.quote);
-      res.writeHead(201, {'Content-Type' : 'application/json'})
-      res.end(JSON.stringify(quotes));
-    });
-  }
+// const handleRequest = function (req, res) {
+//   console.log(`Endpoint: ${req.url} Method: ${req.method}`);
 
-  //CATCH ALL ROUTE
-  else {
-    res.writeHead(404, headers);
-    res.end('Page not found');
-  }
-}
+//   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
+//   if (req.url == '/') {
+//     console.log('redirecting');
+//     res.writeHead(301, { ...headers, Location: `http://localhost:${port}/quote` }) //redirect to quote
+//     res.end();
+//   }
 
-const server = http.createServer(handleRequest);
-server.listen(port);
+//   // TODO: GET ONE
+//   if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
+//     var randomIndex = getRandomInt(0, quotes.length)
+//     req.on('data', (data) => {
+//       res.writeHead(200)
+//     })
+//     req.on('end', () => {
+//       res.end(quotes[randomIndex]);
+//     });
+//   }
 
-console.log('Server is running in the terminal!');
-console.log(`Listening on http://localhost:${port}`);
+//   // TODO: POST/CREATE
+//   else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
+
+//     var data = '';
+//     req.on('data', (chunkOfData) => {
+//       data += chunkOfData.toString();
+//     })
+//     req.on('end', () => {
+//       var quote = JSON.parse(data);
+//       quotes.push(quote.quote);
+//       res.writeHead(201, {'Content-Type' : 'application/json'})
+//       res.end(JSON.stringify(quotes));
+//     });
+//   }
+
+//   //CATCH ALL ROUTE
+//   else {
+//     res.writeHead(404, headers);
+//     res.end('Page not found');
+//   }
+// }
+
+//const server = http.createServer(handleRequest);
+//server.listen(port);
