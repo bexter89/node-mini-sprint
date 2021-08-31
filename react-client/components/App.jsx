0040c99fc89adc1e92e165a7cda6121a63597497
Rebekah Cruz <rebekah.cruz702@gmail.com>
2021-08-31 11:38:18 -0500

    completed react-client

--- react-client/components/App.jsx
+++ react-client/components/App.jsx
@@ -12,13 +12,23 @@ export default class App extends React.Component {
           'one does not simply walk into mordor',
           'here we go, one more time, everybodys feeling fine!'
         ],
+      currentQuote: '',
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getRandomInt = this.getRandomInt.bind(this);
   }
 
   componentDidMount() {
+    this.setState({
+      currentQuote: this.state.quotes[this.getRandomInt(0, this.state.quotes.length)]
+    })
+  }
 
+  getRandomInt (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
   handleSubmit(event) {
@@ -43,6 +53,9 @@ export default class App extends React.Component {
     return (
       <div>
       <h1>Bekah's Random Quote Generator</h1>
+      <span>Your random quote is:</span>
+      <h2>{this.state.currentQuote}</h2>
+      <span>don't like this quote? add one of your own!</span>
       <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
         <label htmlFor="addquote">
         <input type="text" name="addquote" value={this.state.newQuote} placeholder="add a new quote" />
